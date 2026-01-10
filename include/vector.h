79bf5d82af2697c03becff0a14ce2f55a24f6474
vs-123 <19247154+vs-123@users.noreply.github.com>
2026-01-10 13:37:18 +0000

    create mswpr_t struct
    - create `mswpr_t' struct to represent game
    - implement grid with `vector_t'

---
+++ include/vector.h
@@ -0,0 +1,49 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <assert.h>
+#include <stdlib.h>
+
+#define DECLARE_VECTOR(type, name)                                             \
+   typedef struct                                                              \
+   {                                                                           \
+      type *elems;                                                             \
+      unsigned int size;                                                       \
+      unsigned int capacity;                                                   \
+   } name;                                                                     \
+   name name##_new(unsigned int capacity);                                     \
+   void name##_push(name *v, type elem);                                       \
+   type *name##_at(name *v, unsigned int index);                               \
+   void name##_free(name *v);
+
+#define IMPLEMENT_VECTOR(type, name)                                           \
+   name name##_new(unsigned int capacity)                                      \
+   {                                                                           \
+      assert((capacity > 0) && "capacity must be > 0");                        \
+      name v;                                                                  \
+      v.elems    = malloc(sizeof(type) * capacity);                            \
+      v.size     = 0;                                                          \
+      v.capacity = capacity;                                                   \
+      return v;                                                                \
+   }                                                                           \
+   void name##_push(name *v, type elem)                                        \
+   {                                                                           \
+      if (v->size >= v->capacity) {                                            \
+         v->capacity *= 2;                                                     \
+         v->elems = realloc(v->elems, sizeof(type) * v->capacity);             \
+      }                                                                        \
+      v->elems[v->size++] = elem;                                              \
+   }                                                                           \
+   type *name##_at(name *v, unsigned int index)                                \
+   {                                                                           \
+      assert(index < v->size && "Index out of bounds");                        \
+      return &v->elems[index];                                                 \
+   }                                                                           \
+   void name##_free(name *v)                                                   \
+   {                                                                           \
+      free(v->elems);                                                          \
+      v->elems = NULL;                                                         \
+      v->size = v->capacity = 0;                                               \
+   }
+
+#endif /* VECTOR_H */
--- src/main.c
+++ src/main.c
@@ -1,27 +1,140 @@
-#include "raylib.h"
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+
+#include "raylib.h"
+#include "vector.h"
 
 #define SCREEN_WIDTH 16 * 30
 #define SCREEN_HEIGHT 16 * 30 + 50
 
+typedef uint8_t u8;
+
+typedef struct
+{
+   bool is_mine;
+   bool is_revealed;
+   bool has_flag;
+   u8 adjacent_mines; /* won't ever exceed 9 so... u8 seems good enough */
+} cell_t;
+
+DECLARE_VECTOR (cell_t, cell_vector_t)
+DECLARE_VECTOR (cell_vector_t, cell_grid_t)
+
+IMPLEMENT_VECTOR (cell_t, cell_vector_t)
+IMPLEMENT_VECTOR (cell_vector_t, cell_grid_t)
+
+typedef struct
+{
+   u8 grid_columns;
+   u8 grid_rows;
+   u8 cell_size;
+   u8 mines_count;
+   u8 ui_height;
+
+   cell_grid_t cell_grid;
+
+   float start_time;
+   float end_time;
+   float pause_duration;
+   float confirm_start_time;
+
+   bool has_game_ended;
+   bool has_user_won;
+   bool is_reset_confirmed;
+   bool is_first_click;
+
+   Texture2D mine_texture;
+   Texture2D flag_texture;
+
+   Rectangle btn_new_game;
+   Rectangle btn_yes;
+   Rectangle btn_no;
+   Rectangle modal_rect;
+
+   /* colour palette for the numbers */
+   Color colour_palette[9];
+} mswpr_t;
+
+mswpr_t
+mswpr_init (void)
+{
+   mswpr_t mswpr;
+
+   mswpr.grid_columns = 16;
+   mswpr.grid_rows    = 16;
+   mswpr.cell_size    = 30;
+   mswpr.mines_count  = 40;
+   mswpr.ui_height    = 50;
+
+   mswpr.start_time         = GetTime ();
+   mswpr.end_time         = 0.0;
+   mswpr.pause_duration     = 0.0;
+   mswpr.confirm_start_time = 0.0;
+
+   mswpr.has_game_ended     = false;
+   mswpr.has_user_won       = false;
+   mswpr.is_reset_confirmed = false;
+   mswpr.is_first_click     = true;
+
+   mswpr.btn_new_game
+       = (Rectangle){ .x = 10, .y = 10, .width = 105, .height = 30 };
+
+   int modal_width   = 300;
+   int modal_height  = 150;
+   int screen_width  = mswpr.grid_columns * mswpr.cell_size;
+   int screen_height = mswpr.grid_rows * mswpr.cell_size + mswpr.ui_height;
+
+   mswpr.modal_rect
+       = (Rectangle){ .x      = (float)(screen_width / 2 - modal_width / 2),
+                      .y      = (float)(screen_height / 2 - modal_height / 2),
+                      .width  = (float)modal_width,
+                      .height = (float)modal_height };
+
+   mswpr.btn_yes = (Rectangle){ .x      = mswpr.modal_rect.x + 30,
+                                .y      = mswpr.modal_rect.y + modal_height - 50,
+                                .width  = 100,
+                                .height = 30 };
+
+   mswpr.btn_no = (Rectangle){ .x     = mswpr.modal_rect.x + modal_width - 130,
+                               .y     = mswpr.modal_rect.y + modal_height - 50,
+                               .width = 100,
+                               .height = 30 };
+
+   mswpr.mine_texture = LoadTexture ("assets/mine.png");
+   mswpr.flag_texture = LoadTexture ("assets/flag.png");
+
+   Image window_icon = LoadImage ("assets/mine.png");
+   SetWindowIcon (window_icon);
+   UnloadImage (window_icon);
+
+   /* reset_game(); */
+
+   return mswpr;
+}
+
+void
+mswpr_free (mswpr_t *mswpr)
+{
+
+   if (mswpr->mine_texture.width > 0)
+      {
+         UnloadTexture (mswpr->mine_texture);
+      }
+   if (mswpr->flag_texture.width > 0)
+      {
+         UnloadTexture (mswpr->flag_texture);
+      }
+}
+
 int
 main (void)
 {
-   InitWindow (SCREEN_WIDTH, SCREEN_HEIGHT, "Minesweeper -- vs-123");
+   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Minesweeper -- vs-123");
+   
+   mswpr_t mswpr = mswpr_init ();
+   mswpr_free(&mswpr);
 
-   while (!WindowShouldClose ())
-      {
-         if (IsKeyPressed (KEY_ESCAPE))
-            {
-               goto die;
-            }
-         BeginDrawing ();
-         ClearBackground (RAYWHITE);
-         EndDrawing ();
-      }
-
-die:
-   CloseWindow ();
-
+   CloseWindow();
    return 0;
 }
