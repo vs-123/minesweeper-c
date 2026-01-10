cdc2acf151c7b72c520a35beb1eb52cf2bd744b1
vs-123 <19247154+vs-123@users.noreply.github.com>
2026-01-10 12:39:35 +0000

    setup window
    - successfully setup window
    - bound key <ESC> to close the window

--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
-#include <stdio.h>
 #include "raylib.h"
+#include <stdio.h>
+
+#define SCREEN_WIDTH 16 * 30
+#define SCREEN_HEIGHT 16 * 30 + 50
 
 int
 main (void)
 {
-   printf("Hello, minesweeper!");
+   InitWindow (SCREEN_WIDTH, SCREEN_HEIGHT, "Minesweeper -- vs-123");
+
+   while (!WindowShouldClose ())
+      {
+         if (IsKeyPressed (KEY_ESCAPE))
+            {
+               goto die;
+            }
+         BeginDrawing ();
+         ClearBackground (RAYWHITE);
+         EndDrawing ();
+      }
+
+die:
+   CloseWindow ();
+
+   return 0;
 }
